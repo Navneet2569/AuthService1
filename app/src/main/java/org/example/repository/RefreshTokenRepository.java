d80efeb8af7b19c9147f8b837df9da87252d5f03
Navneet Singh <nav9027388401singh@gmail.com>
2026-08-29 22:42:24 +0530

    Added service and repository layers
    - Added JwtService to handle JWT extraction, validation, and token-related operations
    - Added RefreshTokenRepository for persistence and retrieval of refresh tokens
    - Integrated Spring Data CrudRepository for database operations
    - Established the initial service and repository layers for the authentication flow

---
+++ app/src/main/java/org/example/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import java.util.Optional;
+
+import org.example.entities.RefreshToken;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
+    Optional<RefreshToken> findByToken(String token);
+}
---
+++ app/src/main/java/org/example/service/JwtService.java
@@ -0,0 +1,73 @@
+package org.example.service;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    public static final String SECRET = "7mQpK3vN8xR2cL5wY9tF4aH6sD1jG8bV0uZ3nX5kPqE=";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(
+            String token,
+            Function<Claims, T> claimsResolver) {
+
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .signWith(getSignKey())
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
