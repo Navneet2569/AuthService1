c74cd24cd357e3a84a943e3c41153c6ca41bf11f
Navneet Singh <nav9027388401singh@gmail.com>
2026-09-04 23:20:05 +0530

    Installed and started Apache Kafka on my Ubuntu Server

--- app/src/main/java/org/example/controller/TokenController.java
+++ app/src/main/java/org/example/controller/TokenController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+// Installed and started Apache Kafka on my Ubuntu Server
 @Controller
 public class TokenController {
 
